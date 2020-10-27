c3ac92296d7d20bbe3cd587972fd9b5d67223191
scimarin <sergiu128@protonmail.com>
2020-10-27 13:24:12 +0200

    8bitCPU: layout of cpu and memory

--- 3-processor/8bit-CPU/cpu.v
+++ 3-processor/8bit-CPU/cpu.v
@@ -1,2 +1,6 @@
-module cpu;
+module cpu
+    (input wire clk, reset,
+     input wire [7:0] from_memory,
+     output reg write, // can write to memory?
+     output reg [7:0] to_memory)
 endmodule;
--- 3-processor/8bit-CPU/memory.v
+++ 3-processor/8bit-CPU/memory.v
@@ -1,3 +1,17 @@
-module memory;
-// will contain modules for program ROM memory, data RW memory + stuff from the IO ports
+/*
+8 bit address to reference 0..255 locations of 8bits each
+256 total bytes:
+    128: program (ROM)
+    96: data (RW)
+    32: IO (16 in, 16 out)
+entire address is loaded on the bus at once
+*/
+module memory
+    (input wire clk, reset,
+     input wire write, // can write?
+     input wire [15:0] port_in[0:7], // IO
+     input wire [7:0] address, data_in,
+     output reg [7:0] data_out,
+     output reg [15:0] port_out[0:7]); // IO
+
 endmodule;
