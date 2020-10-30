327bffcd5bd9ce4a7fc69711168f8278c72489b2
scimarin <sergiu128@protonmail.com>
2020-10-30 09:48:00 +0200

    fix code format

--- 3-processor/8bit-CPU/data_path.v
+++ 3-processor/8bit-CPU/data_path.v
@@ -1,6 +1,9 @@
-module data_path
-    (input wire clk, reset,
-     input wire [7:0] address,
-     input wire [7:0] from_memory,
-     output reg [7:0] to_memory);
+module data_path(
+    input wire clk, reset,
+    input wire [7:0] address,
+    input wire [7:0] from_memory,
+    output reg [7:0] to_memory
+);
+
+
 endmodule;
